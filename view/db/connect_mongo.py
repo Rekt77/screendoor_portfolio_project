766ad40d10c67295202ccebaba0aba371c9183fe
rekt77 <timtaeil@gmail.com>
2019-05-23 11:43:29 +0000

    modify mongoURL

--- view/db/connect_mongo.py
+++ view/db/connect_mongo.py
@@ -6,7 +6,7 @@ class ConnectDB():
 	def __init__(self):
 		with open("view/db/mongoDB.json") as Json:
 			user_doc = json.loads(Json.read())
-		self.mongoURL = str("mongodb+srv://%s:%s%s"%(user_doc['MongoID'],user_doc['MongoPassword'],user_doc["MongoURL"]))
+		self.mongoURL = str("mongodb://%s:%s%s"%(user_doc['MongoID'],user_doc['MongoPassword'],user_doc["MongoURL"]))
 		self.client = pymongo.MongoClient(self.mongoURL)
 		self.db = pymongo.database.Database(self.client, 'zoin')
 
