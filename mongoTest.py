d683d9e9bebe7a65ae3d7a92f2b3856cf19c28e1
rekt77 <timtaeil@gmail.com>
2019-05-24 14:57:30 +0000

    test

--- mongoTest.py
+++ mongoTest.py
@@ -1,7 +1,7 @@
 import pymongo
 import json
 
-with open("mongoDB.json") as Json:
+with open("./view/db/mongoDB.json") as Json:
     user_doc = json.loads(Json.read())
 
 mongoURL = str("mongodb://%s:%s%s"%(user_doc['MongoID'],user_doc['MongoPassword'],user_doc["MongoURL"]))
@@ -10,4 +10,4 @@ db = pymongo.database.Database(client, 'zoin')
 users = pymongo.collection.Collection(db,'Users')
 
 
-print(users.insert_one({"hi":"1234"}).inserted_id)
+print(users.count())
